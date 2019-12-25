cd7d51417e5d2b03805fe93cf63288594e8ab3e4
jonathanmorais <jonathan.m.lucena@gmail.com>
2019-12-25 17:40:24 -0300

    api: add routes e functions for response

--- main.go
+++ main.go
@@ -1,73 +1,42 @@
 package main
 
 import (
-    "encoding/json"
-    "github.com/gorilla/mux"
-    "log"
-    "net/http"
+   "fmt"
+   "log"
+   "net/http"
+   "github.com/gorilla/mux"
 )
-
-// "Person type" (tipo um objeto)
-type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-    City  string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
-}
-
-var people []Person
-
-// GetPeople mostra todos os contatos da variável people
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(people)
-}
-
-// GetPerson mostra apenas um contato
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for _, item := range people {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(&Person{})
-}
-
-// CreatePerson cria um novo contato
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    var person Person
-    _ = json.NewDecoder(r.Body).Decode(&person)
-    person.ID = params["id"]
-    people = append(people, person)
-    json.NewEncoder(w).Encode(people)
-}
-
-// DeletePerson deleta um contato
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for index, item := range people {
-        if item.ID == params["id"] {
-            people = append(people[:index], people[index+1:]...)
-            break
-        }
-        json.NewEncoder(w).Encode(people)
-    }
-}
-
-// função principal para executar a api
+// primeiro eu declaro uma variavel importando funções newrouter e uma sub-função HandleFunc
+// isso serve como ponte entre o Client e o Server onde pode ser feito requests
+// depois aqui ocorre uma especie de interpolação entre a extremidade  / com alguma da função
+// por ultimo eu coloco um "Listener" para essas requisições serem ouvidas atraves da porta 8080
 func main() {
-    router := mux.NewRouter()
-    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-    router.HandleFunc("/contato", GetPeople).Methods("GET")
-    router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
-    router.HandleFunc("/contato/{id}", CreatePerson).Methods("POST")
-    router.HandleFunc("/contato/{id}", DeletePerson).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8000", router))
+    r := mux.NewRouter().StrictSlash(true)
+    r.HandleFunc("/", Index)
+    r.HandleFunc("/abacate", AbacateIndex)
+    r.HandleFunc("/xuxu", XuxuIndex)
+    r.HandleFunc("/banana/{frutaId}", BananaIndex)
+    log.Fatal(http.ListenAndServe(":8080", r))
+
+}
+
+// função Index onde o lado "Server" responde com alguma coisa
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func AbacateIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome Abacate!")
+}
+
+func XuxuIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome Xuxu!")
+}
+
+// aqui coloquei uma variavel frutaId, com isso eu passo qualquer valor para ela e la em 
+// cima na rota ele vai entender o meu parametro e vai alocar dinamicamente minha varuivel. 
+func BananaIndex(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    frutaId := vars["frutaId"]
+    fmt.Fprintln(w, "Welcome: ", frutaId)
 }
--- scripts/docker-push/docker-push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jonathan99/go-base:1.0
