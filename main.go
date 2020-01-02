63f564f23b10f26815a1ad2799eabb0cd4745a81
jonathanmorais <jonathan.m.lucena@gmail.com>
2020-01-02 15:54:52 -0300

    api routes: main and routes

--- main.go
+++ main.go
@@ -1,65 +1,89 @@
 package main
 
 import (
-   "fmt"
+//    "fmt"
    "log"
    "net/http"
+   "math/rand"
+   "strconv"
    "github.com/gorilla/mux"
    "encoding/json"
 )
 
-type fruta struct {
-    nome string
-    tipo string
-    valor float32
+type Fruta struct {
+    ID    string   `json:"id"`
+    Nome  string   `json:"nome"`
+    Tipo  string   `json:"tipo"`
+    Valor float64  `json:"valor"`
 }
 
-type Frutas []fruta
+var frutas []Fruta
 
 // primeiro eu declaro uma variavel importando funções newrouter e uma sub-função HandleFunc
 // isso serve como ponte entre o Client e o Server onde pode ser feito requests
 // depois aqui ocorre uma especie de interpolação entre a extremidade  / com alguma da função
 // por ultimo eu coloco um "Listener" para essas requisições serem ouvidas atraves da porta 8080
 func main() {
-    r := mux.NewRouter().StrictSlash(true)
-    r.HandleFunc("/", Index)
-    r.HandleFunc("/abacate", AbacateIndex)
-    r.HandleFunc("/xuxu", XuxuIndex)
-    r.HandleFunc("/banana/{frutaId}", BananaIndex)
-    log.Fatal(http.ListenAndServe(":8080", r))
 
+    frutas = append(frutas, Fruta{ID: "1", Nome: "uva", Tipo: "roxa", Valor: 5.8})
+    frutas = append(frutas, Fruta{ID: "2", Nome: "maça", Tipo: "pequena", Valor: 3.6})
+    frutas = append(frutas, Fruta{ID: "3", Nome: "banana", Tipo: "da terra", Valor: 7.0})
+    frutas = append(frutas, Fruta{ID: "4", Nome: "tomate", Tipo: "pequen", Valor: 4.5})
+
+    r := mux.NewRouter()
+    r.HandleFunc("/api/fruta/{id}", getFruta).Methods("GET")
+    r.HandleFunc("/api/fruta/", createFruta).Methods("POST")
+    r.HandleFunc("/api/fruta/{id}", updateFruta).Methods("PUT")
+    r.HandleFunc("/api/fruta/{id}", deleteFruta).Methods("DELETE")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 // função Index onde o lado "Server" responde com alguma coisa
-func Index(w http.ResponseWriter, r *http.Request) {
-    frutas := fruta{
-         nome: "banana",
-         tipo: "banana da terra",
-         valor: 12.0,
-    }
-
-    ret, err := json.Marshal(frutas)
-
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println(string(ret))
-    w.Write(ret)
+func getFruta( w http.ResponseWriter, r *http.Request ) { 
+  w.Header().Set("Content-Type", "application/json") 
+  params := mux.Vars(r)
+  
+  for _, item := range(frutas){
+      if item.ID == params["id"]{
+        json.NewEncoder(w).Encode(item)
+        return    
+      }
+  }
+  json.NewEncoder(w).Encode(&Fruta{})
 }
 
-
-func AbacateIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome Abacate!")
+func createFruta( w http.ResponseWriter, r *http.Request ) {    
+    w.Header().Set("Content-Type", "application/json")
+	var fruta Fruta
+	_ = json.NewDecoder(r.Body).Decode(&fruta)
+	fruta.ID = strconv.Itoa(rand.Intn(1000)) // Mock ID - not safe
+	frutas = append(frutas, fruta)
+	json.NewEncoder(w).Encode(fruta)
 }
 
-func XuxuIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome Xuxu!")
+func updateFruta( w http.ResponseWriter, r *http.Request ) {    
+    params := mux.Vars(r)
+	for index, item := range frutas {
+		if item.ID == params["id"] {
+			frutas = append(frutas[:index], frutas[index+1:]...)
+			var fruta Fruta
+			_ = json.NewDecoder(r.Body).Decode(&fruta)
+			fruta.ID = params["id"]
+			frutas = append(frutas, fruta)
+			json.NewEncoder(w).Encode(fruta)
+			return
+		}
+	}
 }
 
-// aqui coloquei uma variavel frutaId, com isso eu passo qualquer valor para ela e la em 
-// cima na rota ele vai entender o meu parametro e vai alocar dinamicamente minha varuivel. 
-func BananaIndex(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    frutaId := vars["frutaId"]
-    fmt.Fprintln(w, "Welcome: ", frutaId)
+func deleteFruta(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range frutas {
+		if item.ID == params["id"] {
+			frutas = append(frutas[:index], frutas[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(frutas)
 }
